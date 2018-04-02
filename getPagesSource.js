ae2e9932694d1229546b4c0cfd09413e30006433
sujivasagam <sujitha29@gmail.com>
2018-04-02 12:01:59 +0530

    Update getPagesSource.js

--- getPagesSource.js
+++ getPagesSource.js
@@ -27,60 +27,17 @@ for(var i=0; i<l.length; i++) {
             event.initEvent("click", true, true);
             link.dispatchEvent(event);
         }
-        alert(link);
-
-        //else if (link.click) {
+        //alert(link);
             link.click();
-        //}
         link.parentNode.removeChild(link);
-
-
-       /* var element = document.createElement('a');
-       // element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent('text'));
-        element.setAttribute('download', l[i].href);
-
-        element.style.display = 'none';
-        document.body.appendChild(element);
-
-  element.click();
-
-  document.body.removeChild(element);
-
-
-  /*      chrome.downloads.download({
-  url: l[i].href,
-  filename: "suggested/filename/with/relative.path" // Optional*/
-//});
-    //    html+="<a href="+l[i].href+" target='_blank'>"+l[i].href+"</a><br/>";
-       // html+='<iframe width="1" height="1" frameborder="0" src="'+l[i].href+'"></iframe>';
     
     }
-
-        //html+="<a href="+l[i].href+" target='_blank'>"+l[i].href+"</a><br/>";
   arr.push(l[i].href);
 }
 return "Success";
 
     while (node) {
         html+=node+"<br/>";
-       /* switch (node.nodeType) {
-        case Node.ELEMENT_NODE:
-            html += node.outerHTML;
-            break;
-        case Node.TEXT_NODE:
-            html += node.nodeValue;
-            break;
-        case Node.CDATA_SECTION_NODE:
-            html += '<![CDATA[' + node.nodeValue + ']]>';
-            break;
-        case Node.COMMENT_NODE:
-            html += '<!--' + node.nodeValue + '-->';
-            break;
-        case Node.DOCUMENT_TYPE_NODE:
-            // (X)HTML documents are identified by public identifiers
-            html += "<!DOCTYPE " + node.name + (node.publicId ? ' PUBLIC "' + node.publicId + '"' : '') + (!node.publicId && node.systemId ? ' SYSTEM' : '') + (node.systemId ? ' "' + node.systemId + '"' : '') + '>\n';
-            break;
-        }*/
         node = node.nextSibling;
     }
     return html;
